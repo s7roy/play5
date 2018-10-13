8e0dac1a45bae47598583fca1116a8daa89b2611
s7roy <44117712+s7roy@users.noreply.github.com>
2018-10-13 19:52:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,7 +207,7 @@ function play(guild, song) {
 }
 
 client.on('message', message => {
-    var prefix = "4";
+    var prefix = "5";
     
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
